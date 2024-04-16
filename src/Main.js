a755bfdbb9757225ff14bdb40dffaf50cc21a86f
suminkim8203 <kimsm8203@gmail.com>
2024-04-16 13:21:00 +0900

    함수 컴포넌트 이벤트

--- src/Main.js
+++ src/Main.js
@@ -1,34 +1,26 @@
-// buttonimport React, { useState } from "react";
+import React, { useState } from "react";
 
 const Main = () => {
-  const object = { a: 1, b: 2, c: 3 };
-  const nextObject = { ...object, b: 5 }; // object 사본을 만들어서 b 값만 덮어씌운다
-  console.log(nextObject, object);
+  // username 상태
+  const [username, setUsername] = useState("");
+
+  const onChangeUsername = event => {
+    setUsername(event.taget.value);
+    console.log(event.taget.value);
+  };
+
+  return (
+    <div>
+      <h1>이벤트 연습</h1>
+      <input
+        type="text"
+        name="username"
+        placeholder="사용자명"
+        value={username}
+        onChange={onChangeUsername}
+      />
+    </div>
+  );
 };
-
-const array = [
-  { id: 1, value: true },
-  { id: 2, value: true },
-  { id: 3, value: false },
-  { id: 4, value: true },
-  { id: 5, value: false },
-  { id: 6, value: true },
-];
-
-let nextArray = array.concat({ id: 7 }); // 새 항목 추가
-nextArray = nextArray.filter(item => item.id !== 2); // id가 2인 항목 제거
-// id가 1인 항목의 value를 false로 설정
-nextArray = nextArray.map(item =>
-  item.id === 1 ? { ...item, value: false } : item,
-);
-
-console.log(array);
-console.log(nextArray);
-
-<div>
-  {/* <button onClick={onClickEnter}>입장</button>
-      <button onClick={onClickLeave}>퇴장</button>
-      <h1 style={{ color }}>{message}</h1> */}
-</div>;
 
 export default Main;
