87a27091334c8aa21a1583653e0ce7bec7574f80
suminkim8203 <kimsm8203@gmail.com>
2024-04-16 11:58:37 +0900

    docs: useState

--- src/Main.js
+++ src/Main.js
@@ -1,18 +1,34 @@
-import React from "react";
+// buttonimport React, { useState } from "react";
 
-const Main = ({ title, children }) => {
-  // const = props; // props는 객체 = props 안의 속성을 불러옴
-
-  return (
-    <div>
-      <h1>안녕하세요, 저는 {title}입니다.</h1>
-      <h2>children 값은 {children}입니다.</h2>
-    </div>
-  );
+const Main = () => {
+  const object = { a: 1, b: 2, c: 3 };
+  const nextObject = { ...object, b: 5 }; // object 사본을 만들어서 b 값만 덮어씌운다
+  console.log(nextObject, object);
 };
 
-Main.defaultProps = {
-  title: "기본 이름",
-};
+const array = [
+  { id: 1, value: true },
+  { id: 2, value: true },
+  { id: 3, value: false },
+  { id: 4, value: true },
+  { id: 5, value: false },
+  { id: 6, value: true },
+];
+
+let nextArray = array.concat({ id: 7 }); // 새 항목 추가
+nextArray = nextArray.filter(item => item.id !== 2); // id가 2인 항목 제거
+// id가 1인 항목의 value를 false로 설정
+nextArray = nextArray.map(item =>
+  item.id === 1 ? { ...item, value: false } : item,
+);
+
+console.log(array);
+console.log(nextArray);
+
+<div>
+  {/* <button onClick={onClickEnter}>입장</button>
+      <button onClick={onClickLeave}>퇴장</button>
+      <h1 style={{ color }}>{message}</h1> */}
+</div>;
 
 export default Main;
