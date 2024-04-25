d4217ebd6cb47ff2780bef6c7287b2eee2a487df
suminkim8203 <kimsm8203@gmail.com>
2024-04-25 11:10:00 +0900

    axios API 연동

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import CSSModule from "./components/CSSModule";
 import ScssComponents from "./components/ScssComponents";
 import StyledComponent from "./components/StyledComponent";
+import Axios from "./components/pages/Axios";
 
 function App() {
   return (
     <div>
-      {/* <ScssComponents /> */}
-      <CSSModule />
-      <StyledComponent />
+      <Axios />
     </div>
   );
 }
