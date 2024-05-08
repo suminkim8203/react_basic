15282dd41fe325f7f044ed21979f59dc735a6605
suminkim8203 <kimsm8203@gmail.com>
2024-05-08 13:10:26 +0900

    docs: Contexts API

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import CSSModule from "./components/CSSModule";
-import ScssComponents from "./components/ScssComponents";
-import StyledComponent from "./components/StyledComponent";
-import Axios from "./components/pages/Axios";
+import ColorBox from "./components/ColorBox";
+import SelectColors from "./components/SelectColors";
+import { ColorProvider } from "./contexts/colorContext";
 
 function App() {
   return (
-    <div>
-      <Axios />
-    </div>
+    <ColorProvider>
+      <div>
+        <SelectColors />
+        <ColorBox />
+      </div>
+    </ColorProvider>
   );
 }
 
